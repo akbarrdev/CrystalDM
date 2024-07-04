32b79505b6bec0d2c67cf0b2426f074ea9258bdd
akbarrdev <teamnevolution@gmail.com>
2024-07-05 00:09:17 +0700

    Add: under attack mode

--- src/plugins/under_attack.js
+++ src/plugins/under_attack.js
@@ -1,3 +1,18 @@
+import { Utils } from "../library/utils.js";
+import cfg from "../../config.json" assert { type: "json" };
+
 export default async function (fastify, options) {
-  // Kode plugin di sini
+  try {
+    if (cfg.security.underAttack.enabled) {
+    await fastify.register(underPressure, {
+      ...cfg.security.underAttack.settings,
+      pressureHandler: (req, rep, type, value) => {
+        console.log(`Server under pressure: ${type} ${value}`);
+        rep.send(cfg.security.underAttack.settings.message);
+      },
+    });
+  }
+  } catch (err) {
+    Utils.logs("error", err, "Under Attack Plugins", 0);
+  }
 }
