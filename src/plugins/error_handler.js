20abeb283bd053ec6da714cdc3a4ec08adfe01ff
akbarrdev <teamnevolution@gmail.com>
2024-07-13 08:47:24 +0700

    fix: rate limiter

--- src/plugins/error_handler.js
+++ src/plugins/error_handler.js
@@ -3,33 +3,51 @@ import rateLimit from "@fastify/rate-limit";
 import { Utils } from "../library/utils.js";
 import fastifyPlugin from "fastify-plugin";
 
+const blacklistPath = resolve(cfg.security.autoBlacklist.blacklistPath);
+const blacklistData = fs.readFileSync(blacklistPath, "utf-8");
+const blacklist = JSON.parse(blacklistData);
+
 export default fastifyPlugin(async function (fastify, options) {
   try {
-     fastify.addContentTypeParser(
-       "application/x-www-form-urlencoded",
-       (request, payload, done) => {
-         let body = "";
-         payload.on("data", (chunk) => {
-           body += chunk.toString();
-         });
-         payload.on("end", () => {
-           let parsed = {};
-           body.split("&").forEach((pair) => {
-             let [key, value] = pair.split("=");
-             parsed[decodeURIComponent(key)] = decodeURIComponent(value);
-           });
-           done(null, parsed);
-         });
-       }
-     );
+    fastify.addContentTypeParser(
+      "application/x-www-form-urlencoded",
+      (request, payload, done) => {
+        let body = "";
+        payload.on("data", (chunk) => {
+          body += chunk.toString();
+        });
+        payload.on("end", () => {
+          let parsed = {};
+          body.split("&").forEach((pair) => {
+            let [key, value] = pair.split("=");
+            parsed[decodeURIComponent(key)] = decodeURIComponent(value);
+          });
+          done(null, parsed);
+        });
+      }
+    );
 
-    fastify.addHook("onError", (request, reply, error, done) => {
-      Utils.logs(
-        "error",
-        `${error}`,
-        "Error Handler Plugin"
-      );
-      console.log(error);
+    fastify.addHook("onError", async (request, reply, error, done) => {
+      if (error.statusCode === 429) {
+        if (!blacklist.includes(request.ip)) {
+          Utils.logs(
+            "warn",
+            `Rate limit exceeded for IP: ${request.ip}, auto blacklist...`,
+            "Rate Limiter Plugin"
+          );
+          await Utils.sendDiscord("underAttack", { attackerIP: request.ip });
+          blacklist.push(request.ip);
+          try {
+            fs.writeFileSync(blacklistPath, JSON.stringify(blacklist, null, 2));
+          } catch (err) {
+            Utils.logs("error", err, "Auto Blacklist Plugins", 0);
+          }
+        }
+        reply.code(429).send("Too Many Requests");
+      } else {
+        Utils.logs("error", `${error}`, "Error Handler Plugin");
+        console.log(error);
+      }
       done();
     });
 
